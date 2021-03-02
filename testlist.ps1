c9b14f3225a11c4e9af689742f3839d19bd0c13c
Dmitry Gusarov <1462777+gusarov@users.noreply.github.com>
2021-03-01 21:57:37 -0500

    print env

--- testlist.ps1
+++ testlist.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = "Stop"
 Write-Host "##vso[build.updatebuildnumber]$($Env:Build_BuildId)_$($Env:AGENT_NAME)"
 
-cmd /c set
+Get-Item -Path Env:*
 
 docker --version
 docker run --name hw hello-world
