ab1d7fcb67657bd5fa2857ea76ae6d1c80f6611f
Dmitry Gusarov <1462777+gusarov@users.noreply.github.com>
2021-03-01 21:59:50 -0500

    fix casing

--- testlist.ps1
+++ testlist.ps1
@@ -1,5 +1,5 @@
 $ErrorActionPreference = "Stop"
-Write-Host "##vso[build.updatebuildnumber]$($Env:Build_BuildId)_$($Env:AGENT_NAME)"
+Write-Host "##vso[build.updatebuildnumber]$($Env:BUILD_BUILDNUMBER)_$($Env:AGENT_NAME)"
 
 Get-Item -Path Env:*
 
