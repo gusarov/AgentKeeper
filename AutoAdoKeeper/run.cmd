36c33feb4b16f25adcb860dc37be3fe6a235bef5
Dmitry Gusarov <1462777+gusarov@users.noreply.github.com>
2021-03-06 11:42:31 -0500

    use pat from profile

--- AutoAdoKeeper/run.cmd
+++ AutoAdoKeeper/run.cmd
@@ -1,5 +1,5 @@
 docker build -t autoadokeeper:latest .
 docker rm -f autoadokeeper > nul 2>&1
 ::docker run --privileged=true -v /var/run/docker.sock:/var/run/docker.sock -it --name autoadokeeper autoadokeeper %1
-
+set /p PAT= <%UserProfile%\.adoPat
 docker run --env "ADO_URL=https://dev.azure.com/xkit/" --env "PAT=%PAT%" --env "OHN=%COMPUTERNAME%" --detach --restart unless-stopped --privileged=true -v /var/run/docker.sock:/var/run/docker.sock -it --name autoadokeeper autoadokeeper
