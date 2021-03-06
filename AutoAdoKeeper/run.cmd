c2524a1dce12ac1bebad96268565a066522b1c86
Dmitry Gusarov <1462777+gusarov@users.noreply.github.com>
2021-03-06 18:25:44 -0500

    view logs

--- AutoAdoKeeper/run.cmd
+++ AutoAdoKeeper/run.cmd
@@ -3,3 +3,4 @@ docker rm -f autoadokeeper > nul 2>&1
 ::docker run --privileged=true -v /var/run/docker.sock:/var/run/docker.sock -it --name autoadokeeper autoadokeeper %1
 set /p PAT= <%UserProfile%\.adoPat
 docker run --env "ADO_URL=https://dev.azure.com/xkit/" --env "PAT=%PAT%" --env "OHN=%COMPUTERNAME%" --detach --restart unless-stopped --privileged=true -v /var/run/docker.sock:/var/run/docker.sock -it --name autoadokeeper autoadokeeper
+docker logs autoadokeeper -f
