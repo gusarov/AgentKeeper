fa775d192400061b10aafd7a2e2cf34cf102a05e
Dmitry Gusarov <1462777+gusarov@users.noreply.github.com>
2021-10-09 18:43:57 +0300

    fix agent name

--- AutoAdoKeeper/InnerAgent/start.sh
+++ AutoAdoKeeper/InnerAgent/start.sh
@@ -4,15 +4,17 @@ echo "AutoAdo Keeper STARTED!"
 echo $PAT > pat
 echo $OHN
 
-docker build -t autoado:latest --secret id=pat,src=pat --build-arg PAT=$PAT .
-
 loop() {
+	docker build -t autoado$1:latest --secret id=pat,src=pat --build-arg PAT=$PAT --build-arg OHN=$OHN --build-arg ANP=$1 .
+
 	while true; do
 		echo "Stopping..."
 		docker stop autoado$1 > /dev/null 2>&1 || true
 		docker rm -f autoado$1 > /dev/null 2>&1 || true
 		echo "Starting..."
-		docker run -e namePlus=$1 --privileged=true -v /var/run/docker.sock:/var/run/docker.sock --name autoado$1 autoado ./run.sh --once --agent ${OHN}_autoado$1
+		echo autoado$1 
+		echo ${OHN}_autoado$1
+		docker run -e namePlus=$1 --privileged=true -v /var/run/docker.sock:/var/run/docker.sock --name autoado$1 autoado ./run.sh --once
 	done
 }
 
