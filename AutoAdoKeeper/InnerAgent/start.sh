6cd42e43a256f7bc7c39dbbafcdaf7fcfa6c0e81
Dmitry Gusarov <1462777+gusarov@users.noreply.github.com>
2021-10-09 18:29:06 +0300

    add two agents

--- AutoAdoKeeper/InnerAgent/start.sh
+++ AutoAdoKeeper/InnerAgent/start.sh
@@ -6,10 +6,15 @@ echo $OHN
 
 docker build -t autoado:latest --secret id=pat,src=pat --build-arg PAT=$PAT --build-arg OHN=$OHN .
 
-while true; do
-	echo "Stopping..."
-	docker stop autoado > /dev/null 2>&1 || true
-	docker rm -f autoado > /dev/null 2>&1 || true
-	echo "Starting..."
-	docker run --privileged=true -v /var/run/docker.sock:/var/run/docker.sock --name autoado autoado ./run.sh --once
-done
+loop() {
+	while true; do
+		echo "Stopping..."
+		docker stop autoado$1 > /dev/null 2>&1 || true
+		docker rm -f autoado$1 > /dev/null 2>&1 || true
+		echo "Starting..."
+		docker run --privileged=true -v /var/run/docker.sock:/var/run/docker.sock --name autoado$1 autoado ./run.sh --once
+	done
+}
+
+loop &
+loop 2
